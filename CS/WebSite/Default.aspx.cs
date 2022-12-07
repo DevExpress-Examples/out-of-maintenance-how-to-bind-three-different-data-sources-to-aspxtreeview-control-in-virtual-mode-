adb7056551f1f8dda7112de46a8198ca55bd6a3d
DevExpressExampleBot <fakeEmail>
2022-12-07 15:53:10 +0400

    Source auto update [skip ci]

--- CS/WebSite/Default.aspx.cs
+++ CS/WebSite/Default.aspx.cs
@@ -8,7 +8,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
-using DevExpress.Web.ASPxTreeView;
+using DevExpress.Web;
 using System.Collections.Generic;
 
 public partial class _Default : System.Web.UI.Page
--- VB/WebSite/Default.aspx.vb
+++ VB/WebSite/Default.aspx.vb
@@ -9,7 +9,7 @@ Imports System.Web.UI
 Imports System.Web.UI.WebControls
 Imports System.Web.UI.WebControls.WebParts
 Imports System.Web.UI.HtmlControls
-Imports DevExpress.Web.ASPxTreeView
+Imports DevExpress.Web
 Imports System.Collections.Generic
 
 Partial Public Class _Default
